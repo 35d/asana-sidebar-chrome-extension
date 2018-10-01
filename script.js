9d8448b7e4bb44e337fbb508a800923eba87db0f
🌝 Nikke1925 <nikke.934@gmail.com>
2018-10-01 19:26:33 +0900

    :+1: add click event

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-$(function() {});
+$(function() {
+  console.log("Asana Extension");
+
+  // TODO(tsuburaya): キャッシュの対策入れないと初回しかうまく動かないかもしれない
+  const $draggableItemWrapper = $(".BoardColumnCardsContainer-draggableItemWrapper");
+  $draggableItemWrapper.on("click", () => {
+    // console.log($(this));
+    setTimeout(() => {
+      // console.log($(".Token-label").text());
+      // TODO: ↑をvalueにしてローカルストレージに格納する
+      // key をどうするかは要検討
+    }, 3000);
+  });
+});
