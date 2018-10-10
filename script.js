ff50c121c267da19f694197d8b5599590d0fc1f0
🌝 Nikke1925 <nikke.934@gmail.com>
2018-10-10 18:39:07 +0900

    :recycle: add label style

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 $(function() {
   console.log("Asana Extension");
 
-  $('.BoardCardPotPills-potPill').each((index, item) => {
-    console.log(item);
-    const colorKey = item.className.split(' ')[1].replace('BoardCardPotPills-potPill--', '');
-    const labelName = sessionStorage.getItem(colorKey);
-    if (labelName) {
-      const p = document.createElement('p');
-      const nameNode = document.createTextNode(labelName);
-      p.addClass('labelText');
-      p.appendChild(nameNode);
-      item.appendChild(p);
-    }
-  });
+  appendLabelName();
 
-  // TODO(tsuburaya): キャッシュの対策入れないと初回しかうまく動かないかもしれない
   const $draggableItemWrapper = $(".BoardColumnCardsContainer-draggableItemWrapper");
   $draggableItemWrapper.on("click", () => {
     // console.log($(this));
     setTimeout(() => {
-      const key = $('.Tokenizer-token').attr('class').split(' ')[1].replace('Token--', '');
-      const value = $('.Token-label').text();
-      sessionStorage.setItem(key, value.toString());
-      // TODO: ↑をvalueにしてローカルストレージに格納する
-      // key をどうするかは要検討
+      const key = $(".Tokenizer-token")
+        .attr("class")
+        .split(" ")[1]
+        .replace("Token--", "");
+      const value = $(".Token-label").text();
+      localStorage.setItem(key, value.toString());
+      // appendLabelName();
     }, 3000);
   });
 });
+
+appendLabelName = () => {
+  $(".BoardCardPotPills-potPill").each((index, item) => {
+    const colorKey = item.className.split(" ")[1].replace("BoardCardPotPills-potPill--", "");
+    const labelName = localStorage.getItem(colorKey);
+    if (labelName) {
+      const p = document.createElement("p");
+      const nameNode = document.createTextNode(labelName);
+      p.setAttribute("class", "labelText");
+      p.appendChild(nameNode);
+      item.appendChild(p);
+    }
+  });
+};
